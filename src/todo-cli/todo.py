98816da56c28506e40b28a3bfbabeeb8bb20f4b7
eliasskau <eliasskau@gmail.com>
2026-06-25 10:33:47 +0200

    changed db location

--- src/todo-cli/todo.py
+++ src/todo-cli/todo.py
@@ -1,14 +1,25 @@
 import click
+import sqlite3
+
+con = sqlite3.connect("../../data/todo.db")
+cur = con.cursor()
+cur.execute("CREATE TABLE todo(item)")
 
 @click.group()
 def main():
     """ A private cli to do list"""
     pass
-@click.command()
-@click.option('--num',prompt='How much do you have to do?',help='How many todos does the user have')
-def todo(num):
-    click.echo(f"You have {num} things to do")
 
+@main.command()
+@click.option('--item',prompt='Add an item to your todo list',help='Add items to a todo SQLite database')
+def add(item):
+    cur.execute("""INSERT INTO todo VALUES (?),item""")
+    con.commit()
+
+@main.command()
+def list():
+    for row in cur.execute("SELECT item FROM todo"):
+        print(row)
 
 
 
